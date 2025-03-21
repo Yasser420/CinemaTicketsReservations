a91a9aa85145a8e790946e8ea40466d9aa7e7ab6
yasser-Hamody <yasser.hamody@extern.intenta.de>
2025-03-21 15:27:41 +0300

    Adding class based view for getting and posting guest

--- tickets/views.py
+++ tickets/views.py
@@ -5,41 +5,61 @@ from .serializers import MovieSerializer ,ReservationSerializer , GuestSerialize
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 # Create your views here.
 #function based view 
 #the get_guests_rest function is used for both GET and POST methods for the same endpoint with different business logic
-@api_view(['GET','POST'])
-def get_guests(request):
-     #if the request is GET method
-     if request.method== 'GET': 
-         guests= Guest.objects.all()
-         serializer = GuestSerializer(guests,many=True)
-         return Response(serializer.data , status=status.HTTP_200_OK)
-     elif request.method == 'POST':    
-       serializer = GuestSerializer(data=request.data)
-       #if the request data is valid
-       if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-       #if request data is not valid
-       return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['GET','POST'])
+# def get_guests(request):
+#      #if the request is GET method
+#      if request.method== 'GET': 
+#          guests= Guest.objects.all()
+#          serializer = GuestSerializer(guests,many=True)
+#          return Response(serializer.data , status=status.HTTP_200_OK)
+#      elif request.method == 'POST':    
+#        serializer = GuestSerializer(data=request.data)
+#        #if the request data is valid
+#        if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data,status=status.HTTP_201_CREATED)
+#        #if request data is not valid
+#        return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
          
-#GET PUT DELETE
-@api_view(['GET','PUT','DELETE'])
-def guest(request, pk):
-    try:
-      _guest=Guest.objects.get(pk=id)
-    except Guest.DoesNotExist:
-         return Response(status=status.HTTP_404_NOT_FOUND)    
-    if request.method== 'GET':
-      serializer=GuestSerializer(_guest)
-      return Response(serializer.data)
-    elif request.method=='PUT':
-      serializer=GuestSerializer(_guest,data=request.data)
-      if serializer.is_valid():
-           serializer.save()
-           return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
-      return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    elif request.method=='DELETE':
-         _guest=Guest.delete()
-         return Response(status=status.HTTP_204_NO_CONTENT)
+# #GET PUT DELETE
+# @api_view(['GET','PUT','DELETE'])
+# def guest(request, pk):
+#     try:
+#       _guest=Guest.objects.get(pk=pk)
+#     except Guest.DoesNotExist:
+#          return Response(status=status.HTTP_404_NOT_FOUND)    
+#     if request.method== 'GET':
+#       serializer=GuestSerializer(_guest)
+#       return Response(serializer.data)
+#     elif request.method=='PUT':
+#       serializer=GuestSerializer(_guest,data=request.data)
+#       if serializer.is_valid():
+#            serializer.save()
+#            return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
+#       return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method=='DELETE':
+#          _guest.delete()
+#          return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+#use class based views for GET List and POST object
+
+#get list of guests
+class guests(APIView):
+     def get(self, request): 
+         guests=Guest.objects.all()
+         serializer=GuestSerializer(guests,many=True)
+         return Response(serializer.data,status=status.HTTP_200_OK)
+     
+     def post(self,request):
+         serializer=GuestSerializer(data=request.data)   
+         if serializer.is_valid():
+              serializer.save()
+              return Response(serializer.data,status=status.HTTP_201_CREATED)
+         return Response({"errors": serializer.errors},status=status.HTTP_400_BAD_REQUEST)  

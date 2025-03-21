432f8948bad279cde4f8cc93994c565025788d70
yasser-Hamody <yasser.hamody@extern.intenta.de>
2025-03-21 16:49:04 +0300

    Adding url for cbv pk

--- project/urls.py
+++ project/urls.py
@@ -5,5 +5,5 @@ from tickets import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('guests/', views.guests.as_view()),
-   # path('guests/<int:pk>',views.guest)
+    path('guests/<int:pk>',views.CBV_PK().as_view())
 ]

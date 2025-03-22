e0eca75a95501f1a0510f85aee484f0ebfd18e7e
yasser-Hamody <yasser.hamody@extern.intenta.de>
2025-03-22 14:17:32 +0300

    Adding new url and register a router for view set

--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,14 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from tickets import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('guests', views.guest_CRUD)
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('guests/', views.guests.as_view()),
-    path('guests/<int:pk>',views.CBV_PK().as_view())
+    # path('guests/', views.guests.as_view()),
+    # path('guests/<int:pk>',views.CBV_PK().as_view())
+    path('api1/', include(router.urls))
 ]

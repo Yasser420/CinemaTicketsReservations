0708d2b0d30f393f5c5d69ed8da2619b468eefc7
yasser-Hamody <yasser.hamody@extern.intenta.de>
2025-03-23 17:38:17 +0300

    include the tickets urls in main urls for project

--- project/urls.py
+++ project/urls.py
@@ -1,19 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path , include
-from tickets import views
-from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register('guests', views.guest_CRUD)
-router.register('movies', views.movie_CRUD)
-router.register('reservations', views.reservation_CRUD)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('search/movie', views.find_movie) , 
-    # path('guests/', views.guests.as_view()),
-    # path('guests/<int:pk>',views.CBV_PK().as_view())
-    path('api1/', include(router.urls))
-   
+    path('api/',include('tickets.urls'))
 ]

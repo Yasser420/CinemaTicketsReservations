729bbdba6ff4e31941f66c4e3cb73133b0a3a9a1
yasser-Hamody <yasser.hamody@extern.intenta.de>
2025-03-23 17:39:25 +0300

    Modify the Guest model name / make it User

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Movie ,Reservation , Guest
+from .models import Movie ,Reservation , User
 # Register your models here.
 admin.site.register(Movie)
 admin.site.register(Reservation)
-admin.site.register(Guest)
+admin.site.register(User)

7480605345f0ab7ddd8961f64bf5303403ef2a7c
yasser-Hamody <yasser.hamody@extern.intenta.de>
2025-03-23 15:38:38 +0300

    Alert the models by adding User model and edit other models fields

--- tickets/models.py
+++ tickets/models.py
@@ -1,9 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
-class Guest(models.Model):
-    name=models.CharField(max_length=10)
-    phone=models.CharField(max_length=13)
+class User(AbstractUser):
+   ROLE_CHOICES = [
+       ('user' , 'User') , 
+       ('admin','Admin') , 
+       ('super admin' , 'Super Admin')
+   ]
+   role = models.CharField(max_length=20 , choices=ROLE_CHOICES , default='user')
 
 class Movie(models.Model):
     name=models.CharField(max_length=20)
@@ -11,6 +15,6 @@ class Movie(models.Model):
     duration=models.PositiveIntegerField(help_text="Duration in minutes")
     
 class Reservation(models.Model):
-    guest=models.ForeignKey(Guest, related_name='reservation',on_delete=models.CASCADE)
+    user=models.ForeignKey(User, related_name='reservation',on_delete=models.CASCADE)
     movie=models.ForeignKey(Movie, related_name='reservation',on_delete=models.CASCADE)
     reservation_date=models.DateTimeField(null=False)

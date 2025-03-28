1257b7fda0ad030883eb1aba44bf6358aa3de689
yasser-Hamody <yasser.hamody@extern.intenta.de>
2025-03-28 09:43:49 +0300

    edit the name convention for user and admin registeration

--- tickets/urls.py
+++ tickets/urls.py
@@ -2,9 +2,9 @@
 from django.contrib import admin
 from django.urls import path , include
 from rest_framework_simplejwt.views import TokenObtainPairView , TokenRefreshView
-from .views import UserRegsiterView ,AddAdminView
+from .views import RegisterUserView ,AddAdminView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/register/', UserRegsiterView.as_view()),
+    path('user/register/', RegisterUserView.as_view()),
     path('admins/adding/', AddAdminView.as_view())
 ]

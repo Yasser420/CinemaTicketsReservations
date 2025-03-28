4ad174146f9c5eb2b90e64c71d97d0b6905ad39c
yasser-Hamody <yasser.hamody@extern.intenta.de>
2025-03-28 12:17:55 +0300

    Adding urls for super manage users and admins by super admins

--- tickets/urls.py
+++ tickets/urls.py
@@ -2,9 +2,12 @@
 from django.contrib import admin
 from django.urls import path , include
 from rest_framework_simplejwt.views import TokenObtainPairView , TokenRefreshView
-from .views import RegisterUserView ,AddAdminView
+from .views import RegisterUserView ,AddAdminView , SuperAdminUsersView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/register/', RegisterUserView.as_view()),
-    path('admins/adding/', AddAdminView.as_view())
+    path('users/register/', RegisterUserView.as_view()),
+    path('admins/adding/', AddAdminView.as_view()), 
+    path('users/',SuperAdminUsersView.as_view() ) ,
+    path('users/<int:pk>/',SuperAdminUsersView.as_view() )
+    
 ]

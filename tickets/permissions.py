eecea0d7f1903b9f97caefda8202dac99b2daff1
yasser-Hamody <yasser.hamody@extern.intenta.de>
2025-03-28 12:07:27 +0300

    Adding IsAdmin permission

--- tickets/permissions.py
+++ tickets/permissions.py
@@ -2,29 +2,25 @@ from rest_framework import permissions
 from rest_framework.exceptions import PermissionDenied
 from django.contrib.auth import get_user_model
 
-User = get_user_model()  # Gets your custom User model
+User = get_user_model()  
 
 class IsSuperAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        if not request.user.is_authenticated: 
-            raise PermissionDenied(
-                {
-                    "status": "error",
-                    "code": "not_authenticated",
-                    "message": "Authentication credentials were not provided."
-                },
-                code=401
-            )
         
         user  = User.objects.get(id = request.user.id)
         
         if user.role !="super admin":
             raise PermissionDenied(
                 {
-                    "status": "error",
-                    "code": "permission_denied",
                     "message":"You must be super admin to perform this operation "
                 },
-                code=403
+
             )
+        return True
+    
+class IsAdmin(permissions.BasePermission):
+     def has_permission(self, request, view):
+        user = User.objects.get(id = request.user.id)
+        if user.role == 'user' : 
+            raise PermissionDenied({"message":"You must have admin role to perform this operation"})
         return True
